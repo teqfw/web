50ba1fc51050cc96660c360074f50fcf349c9f31
Alex Gusev <alex@flancer64.com>
2021-07-13 13:33:52 +0300

    WF-466 Habr: @teqfw/core

--- src/Back/Cli/Server/Start.mjs
+++ src/Back/Cli/Server/Start.mjs
@@ -39,6 +39,7 @@ function Factory(spec) {
      * @memberOf TeqFw_Web_Back_Cli_Server_Start
      */
     const action = async function () {
+        logger.pause(false);
         logger.info('Starting HTTP/1 server.');
         try {
             /**
