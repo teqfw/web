c0195c68c018282c98102172bb3e967e96ea8b8c
Alex Gusev <alex@flancer64.com>
2021-11-16 10:30:02 +0200

    DC-39 Close SSE connection on the server

--- src/Front/SSE/Connect.mjs
+++ src/Front/SSE/Connect.mjs
@@ -13,6 +13,8 @@ export default class TeqFw_Web_Front_SSE_Connect {
         // EXTRACT DEPS
         /** @type {TeqFw_Web_Front_Defaults} */
         const DEF = spec['TeqFw_Web_Front_Defaults$'];
+        /** @type {TeqFw_Core_Shared_Logger} */
+        const logger = spec['TeqFw_Core_Shared_Logger$'];
         /** @type {TeqFw_Core_Shared_Util.isObject|function} */
         const isObject = spec['TeqFw_Core_Shared_Util.isObject'];
         /** @type {TeqFw_Web_Front_Api_SSE_IState} */
@@ -28,6 +30,7 @@ export default class TeqFw_Web_Front_SSE_Connect {
             if (_source && (_source.readyState !== SSE_STATE.CLOSED)) {
                 _source.close();
                 state.closed();
+                logger.info(`SSE connection is closed.`);
             }
         }
 
@@ -43,12 +46,19 @@ export default class TeqFw_Web_Front_SSE_Connect {
                 await new Promise((resolve) => {
                     _source = new EventSource(url);
                     _source.addEventListener('open', function () {
+                        logger.info(`New SSE connection is opened.`);
                         resolve();
                     });
                     _source.addEventListener('error', function (e) {
-                        state.error(e);
-                        me.close();
+                        if (e.eventPhase === EventSource.CLOSED) {
+                            me.close();
+                        } else {
+                            state.error(e);
+                            logger.error(`Error in SSE connection (event: ${JSON.stringify(e)}).`);
+                            me.close();
+                        }
                     });
+                    // add event handlers to connection
                     if (typeof handlers === 'function') {
                         _source.addEventListener('message', handlers);
                     } else if (isObject(handlers)) {
