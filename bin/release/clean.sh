d6a546c0ec435f6a4509faa4576cd1e21d09ba88
Alex Gusev <alex@flancer64.com>
2021-12-20 12:56:23 +0200

    WF-536 Rollout new versions for TeqFW plugins

--- bin/release/clean.sh
+++ bin/release/clean.sh
@@ -4,8 +4,12 @@
 ##
 DIR_ROOT=${DIR_ROOT:-$(cd "$(dirname "$0")/../../" && pwd)}
 
+rm "${DIR_ROOT}/.babelrc"
+rm "${DIR_ROOT}/.eslintrc.js"
+rm "${DIR_ROOT}/bin/tequila.mjs"
 rm -fr "${DIR_ROOT}/doc/"
 rm -fr "${DIR_ROOT}/node_modules/"
 rm -fr "${DIR_ROOT}/package-lock.json"
 rm -fr "${DIR_ROOT}/test/"
 rm -fr "${DIR_ROOT}/tmp/"
+rm -fr "${DIR_ROOT}/var/"
