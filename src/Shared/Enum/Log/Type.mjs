31c4dfadf081dac8aeb39451726720cef7cdcaad
Alex Gusev <alex@flancer64.com>
2022-02-19 08:07:22 +0200

    WF-575 Add front and back markers to log entries

---
+++ src/Shared/Enum/Log/Type.mjs
@@ -0,0 +1,10 @@
+/**
+ * Enumeration for types of log entries.
+ */
+const TeqFw_Web_Shared_Enum_Log_Type = {
+    BACK: 'back', // log from backend part of app
+    FRONT: 'front', // log from frontend part of app
+}
+
+Object.freeze(TeqFw_Web_Shared_Enum_Log_Type);
+export default TeqFw_Web_Shared_Enum_Log_Type;
