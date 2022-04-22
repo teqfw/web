429099258c4c7c4138a58d1024ded3df922fdf8e
Alex Gusev <alex@flancer64.com>
2022-04-22 15:24:52 +0300

    WF-588 Extract events from web plugin

--- src/Back/Plugin/Init.mjs
+++ src/Back/Plugin/Init.mjs
@@ -7,12 +7,11 @@ const NS = 'TeqFw_Web_Back_Plugin_Init';
 export default function Factory(spec) {
     // DEPS
     /** @type {TeqFw_Di_Shared_Container} */
-    const container = spec['TeqFw_Di_Shared_Container$'];
+    // const container = spec['TeqFw_Di_Shared_Container$'];
 
     // FUNCS
     async function action() {
-        await container.get('TeqFw_Web_Event_Back_Hand_Front_Authenticate$');
-        await container.get('TeqFw_Web_Event_Back_Hand_Server_Key_Source$');
+
     }
 
     // MAIN
--- src/Shared/Defaults.mjs
+++ src/Shared/Defaults.mjs
@@ -6,8 +6,6 @@ export default class TeqFw_Web_Shared_Defaults {
     CFG_APP = '/app';
     CFG_DI = '/di';
 
-    EVENT_AUTHENTICATE = 'authenticate'; // SSE event name
-
     LOG_META_TYPE = 'type'; // log type in log metadata sent to logs monitor (TODO: move to log plugin)
 
     NAME = '@teqfw/web';
@@ -17,14 +15,9 @@ export default class TeqFw_Web_Shared_Defaults {
 
     // URL prefix for API requests: https://.../door/space/...
     SPACE_CFG = 'cfg'; // configuration data (namespaces for DI, etc)
-    SPACE_EVENT_DIRECT = 'efb'; // events stream from front to back
-    SPACE_EVENT_REVERSE = 'ebf'; // events stream from back to front
     SPACE_SRC = 'src'; // sources from 'node_modules'
     SPACE_UPLOAD = 'upload';
     SPACE_WEB = 'web';
-
-    // TODO: set dev 64 sec to prod 16 sec.
-    TIMEOUT_EVENT_RESPONSE = 64000; // default timeout for response event (sent from back as answer to request from front)
 
     constructor(spec) {
         this.MOD_CORE = spec['TeqFw_Core_Shared_Defaults$'];
