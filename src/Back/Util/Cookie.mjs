c8c820a9c89ad00b1af43db263725db2e46de5d5
Alex Gusev <alex@flancer64.com>
2023-05-10 16:20:02 +0300

    MF-139 Add authenticator on backend

--- src/Back/Util/Cookie.mjs
+++ src/Back/Util/Cookie.mjs
@@ -1,20 +1,23 @@
 /**
- * Utilities related to HTTP2 server.
- * @namespace TeqFw_Web_Back_Util
+ * Utilities related to the web server.
+ * @namespace TeqFw_Web_Back_Util_Cookie
  */
-// DEFINE WORKING VARS
-const NS = 'TeqFw_Web_Back_Util';
+// MODULE'S IMPORTS
+import {constants as H2} from 'node:http2';
 
+// MODULE'S VARS
+const NS = 'TeqFw_Web_Back_Util_Cookie';
+
+// MODULE'S FUNCS
 /**
  * Compose expired HTTP cookie to remove existing cookie with the same name in the browser.
  *
  * @param {string} name
  * @param {string} path
  * @returns {string}
- * @memberOf TeqFw_Web_Back_Util
- * @deprecated use TeqFw_Web_Back_Util_Cookie.clear
+ * @memberOf TeqFw_Web_Back_Util_Cookie
  */
-function cookieClear({name, path = '/'}) {
+function clear({name, path = '/'}) {
     // MAIN
     const exp = 'Expires=Thu, 01 Jan 1970 00:00:00 GMT';
     return `${name}=; ${exp}; Path=${path}`;
@@ -32,10 +35,9 @@ function cookieClear({name, path = '/'}) {
  * @param {boolean} httpOnly
  * @param {string} sameSite [Lax | Strict | None] @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
  * @returns {string}
- * @memberOf TeqFw_Web_Back_Util
- * @deprecated use TeqFw_Web_Back_Util_Cookie.create
+ * @memberOf TeqFw_Web_Back_Util_Cookie
  */
-function cookieCreate({name, value, path, expires, domain, secure, httpOnly, sameSite}) {
+function create({name, value, path, expires, domain, secure, httpOnly, sameSite}) {
     let result = `${name}=${value};`;
     if (expires) {
         if (expires instanceof Date) {
@@ -58,11 +60,45 @@ function cookieCreate({name, value, path, expires, domain, secure, httpOnly, sam
     return result;
 }
 
+/**
+ * Get cookie from HTTP request.
+ * @param {IncomingMessage|Http2ServerRequest} request
+ * @param {string} cookie cookie name
+ * @returns {string|null}
+ * @memberOf TeqFw_Web_Back_Util_Cookie
+ */
+function get({request, cookie}) {
+    const headers = request.headers;
+    const cookies = headers[H2.HTTP2_HEADER_COOKIE];
+    const value = cookies.match('(^|;)\\s*' + cookie + '\\s*=\\s*([^;]+)')?.pop() || '';
+    if (value.length) {
+        // there is session cookie in HTTP request
+        return value;
+    }
+    return null;
+}
+
+/**
+ * Set cookie to HTTP response.
+ * @param {ServerResponse|Http2ServerResponse} response
+ * @param {string} cookie cookie value (like `${name}=${value};`)
+ * @memberOf TeqFw_Web_Back_Util_Cookie
+ */
+function set({response, cookie}) {
+    const cookies = response.getHeader(H2.HTTP2_HEADER_SET_COOKIE);
+    const value = (cookies) ? `${cookies}${cookie}` : cookie;
+    response.setHeader(H2.HTTP2_HEADER_SET_COOKIE, value);
+}
+
 // finalize code components for this es6-module
-Object.defineProperty(cookieClear, 'namespace', {value: NS});
-Object.defineProperty(cookieCreate, 'namespace', {value: NS});
+Object.defineProperty(clear, 'namespace', {value: NS});
+Object.defineProperty(create, 'namespace', {value: NS});
+Object.defineProperty(get, 'namespace', {value: NS});
+Object.defineProperty(set, 'namespace', {value: NS});
 
 export {
-    cookieClear,
-    cookieCreate
+    clear,
+    create,
+    get,
+    set,
 };
