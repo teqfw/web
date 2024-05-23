10abf91678107df0fdfbba9d5bf0faba56cd5526
Alex Gusev <alex@flancer64.com>
2024-05-23 07:27:13 +0300

    LP-92 Use lv & ru langs on the sign up form of the cust app

--- src/Front/Mod/Version.mjs
+++ src/Front/Mod/Version.mjs
@@ -3,11 +3,13 @@
  */
 export default class TeqFw_Web_Front_Mod_Version {
     /**
+     * @param {TeqFw_Core_Shared_Api_Logger} logger -  instance
      * @param {TeqFw_Web_Front_Store_Local_Version} storeVersion
      * @param {TeqFw_Web_Front_Mod_Config} modCfg
      */
     constructor(
         {
+            TeqFw_Core_Shared_Api_Logger$$: logger,
             TeqFw_Web_Front_Store_Local_Version$: storeVersion,
             TeqFw_Web_Front_Mod_Config$: modCfg,
         }
@@ -25,11 +27,13 @@ export default class TeqFw_Web_Front_Mod_Version {
             } else {
                 // the first run, save the version and continue
                 storeVersion.set(onServer);
+                logger.info(`Store the current version '${onServer}' locally.`);
                 return false;
             }
         };
 
         this.storeReset = function () {
+            logger.info(`The local store is cleared.`);
             return storeVersion.clear();
         };
         this.versionInstalled = function () {
