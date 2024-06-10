344b8479369240d0796177bfe5c8dd7a564d9680
Alex Gusev <alex@flancer64.com>
2024-06-10 18:23:43 +0300

    lp-desk-12 Add restrictions to the listings
    link to issue: flancer32/lp-desk#12

--- src/Back/App/Server/Respond.mjs
+++ src/Back/App/Server/Respond.mjs
@@ -20,9 +20,11 @@ const {
     HTTP_STATUS_NOT_FOUND,
 } = H2;
 
+// MODULE'S FUNCS
 /**
  * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
  * @memberOf TeqFw_Web_Back_App_Server_Respond
+ * @deprecated use the class form of the dependency injection
  */
 function respond400(res) {
     if (!res.headersSent) {
@@ -37,13 +39,14 @@ function respond400(res) {
  * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
  * @param {string} err
  * @memberOf TeqFw_Web_Back_App_Server_Respond
+ * @deprecated use the class form of the dependency injection
  */
 function respond403(res, err) {
     if (!res.headersSent) {
         res.writeHead(HTTP_STATUS_FORBIDDEN, {
             [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
         });
-        res.write('Access to resource is forbidden. ');
+        res.write('Access to the resource is forbidden. ');
         if (typeof err === 'string') res.write(err);
         res.end();
     }
@@ -53,13 +56,14 @@ function respond403(res, err) {
  * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
  * @param {string} [err]
  * @memberOf TeqFw_Web_Back_App_Server_Respond
+ * @deprecated use the class form of the dependency injection
  */
 function respond404(res, err) {
     if (!res.headersSent) {
         res.writeHead(HTTP_STATUS_NOT_FOUND, {
             [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
         });
-        res.write('Requested resource is not found. ');
+        res.write('Requested resource not found. ');
         if (typeof err === 'string') res.write(err);
         res.end();
     }
@@ -68,6 +72,7 @@ function respond404(res, err) {
 /**
  * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
  * @memberOf TeqFw_Web_Back_App_Server_Respond
+ * @deprecated use the class form of the dependency injection
  */
 function respond405(res) {
     if (!res.headersSent) {
@@ -84,6 +89,7 @@ function respond405(res) {
  * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
  * @param {string} err
  * @memberOf TeqFw_Web_Back_App_Server_Respond
+ * @deprecated use the class form of the dependency injection
  */
 function respond500(res, err) {
     if (!res.headersSent) {
@@ -93,6 +99,85 @@ function respond500(res, err) {
         res.write('Internal server error. ');
         if (typeof err === 'string') res.write(err);
         res.end();
+    }
+}
+
+// MODULE'S CLASSES
+export default class TeqFw_Web_Back_App_Server_Respond {
+    /**
+     * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
+     * @memberOf TeqFw_Web_Back_App_Server_Respond
+     */
+    respond400(res) {
+        if (!res.headersSent) {
+            res.writeHead(HTTP_STATUS_BAD_REQUEST, {
+                [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
+            });
+            res.end('Malformed request syntax.');
+        }
+    }
+
+    /**
+     * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
+     * @param {string} err
+     * @memberOf TeqFw_Web_Back_App_Server_Respond
+     */
+    respond403(res, err) {
+        if (!res.headersSent) {
+            res.writeHead(HTTP_STATUS_FORBIDDEN, {
+                [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
+            });
+            res.write('Access to the resource is forbidden. ');
+            if (typeof err === 'string') res.write(err);
+            res.end();
+        }
+    }
+
+    /**
+     * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
+     * @param {string} [err]
+     * @memberOf TeqFw_Web_Back_App_Server_Respond
+     */
+    respond404(res, err) {
+        if (!res.headersSent) {
+            res.writeHead(HTTP_STATUS_NOT_FOUND, {
+                [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
+            });
+            res.write('Requested resource not found. ');
+            if (typeof err === 'string') res.write(err);
+            res.end();
+        }
+    }
+
+    /**
+     * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
+     * @memberOf TeqFw_Web_Back_App_Server_Respond
+     */
+    respond405(res) {
+        if (!res.headersSent) {
+            const allowed = `${HTTP2_METHOD_HEAD}, ${HTTP2_METHOD_GET}, ${HTTP2_METHOD_POST}`;
+            res.writeHead(HTTP_STATUS_METHOD_NOT_ALLOWED, {
+                [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
+                [HTTP2_HEADER_ALLOW]: allowed,
+            });
+            res.end(`Requested method is not allowed. Allowed methods: ${allowed}.`);
+        }
+    }
+
+    /**
+     * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res
+     * @param {string} err
+     * @memberOf TeqFw_Web_Back_App_Server_Respond
+     */
+    respond500(res, err) {
+        if (!res.headersSent) {
+            res.writeHead(HTTP_STATUS_INTERNAL_SERVER_ERROR, {
+                [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain',
+            });
+            res.write('Internal server error. ');
+            if (typeof err === 'string') res.write(err);
+            res.end();
+        }
     }
 }
 
@@ -112,4 +197,4 @@ export {
     respond404,
     respond405,
     respond500,
-}
+};
