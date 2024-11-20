1f756240c76d485fb184ae174c90beb231771510
Alex Gusev <alex@flancer64.com>
2024-11-20 11:16:05 +0200

    RA-501 Generate own UUID for old browsers

--- src/Front/Util/Crypto.mjs
+++ src/Front/Util/Crypto.mjs
@@ -2,12 +2,28 @@
  * Frontend implementation of TeqFw_Core_Shared_Api_Util_Crypto.
  *
  * @namespace TeqFw_Web_Front_Util_Crypto
+ * @deprecated I think we should use a polyfills
  */
 // MODULE'S IMPORT
 const NS = 'TeqFw_Web_Front_Util_Crypto';
 
+/**
+ * @memberOf TeqFw_Web_Front_Util_Crypto
+ * @return {string}
+ */
 function randomUUID() {
-    return self.crypto.randomUUID();
+    if (self.crypto && typeof self.crypto.randomUUID === 'function') {
+        return self.crypto.randomUUID();
+    } else {
+        const template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+        return template.replace(/[xy]/g, (c) => {
+            const r = (self.crypto && self.crypto.getRandomValues)
+                ? (self.crypto.getRandomValues(new Uint8Array(1))[0] & 15)
+                : Math.floor(Math.random() * 16);
+            const v = c === 'x' ? r : (r & 0x3) | 0x8;
+            return v.toString(16);
+        });
+    }
 }
 
 // MAIN
@@ -15,4 +31,4 @@ Object.defineProperty(randomUUID, 'namespace', {value: NS});
 
 export {
     randomUUID,
-}
+};
